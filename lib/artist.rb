c1389217c1dfa4335c969faa90bc389ffdc28ca2
connor worthen <connorw220@protonmail.com>
2019-11-21 23:14:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,22 @@
+require "pry"
 class Artist
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @songs = []
-    end
   end
-end
+
+  def songs
+    Song.all.find_all {|song| song.artist == self}
+  end
+  #
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    Song.new(name).artist = self
+  end
 
 end
